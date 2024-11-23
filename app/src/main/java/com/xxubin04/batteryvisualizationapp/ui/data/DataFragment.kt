bf93923431d5646017563b5d83506d65c2807c74
xxubin04 <yubinjeong40@gmail.com>
2024-11-23 12:48:19 +0900

    Add: 뒤로가기 버튼 및 이동 로직 구현

--- app/src/main/java/com/xxubin04/batteryvisualizationapp/ui/data/DataFragment.kt
+++ app/src/main/java/com/xxubin04/batteryvisualizationapp/ui/data/DataFragment.kt
@@ -1,13 +1,20 @@
 package com.xxubin04.batteryvisualizationapp.ui.data
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.xxubin04.batteryvisualizationapp.databinding.FragmentDataBinding
+import com.xxubin04.batteryvisualizationapp.ui.home.HomeActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class DataFragment : Fragment() {
 
@@ -24,6 +31,19 @@ class DataFragment : Fragment() {
 
         _binding = FragmentDataBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        val toolbar: Toolbar = binding.toolbar
+        toolbar.setNavigationIcon(com.xxubin04.batteryvisualizationapp.R.drawable.ic_arrow_back)
+        toolbar.setNavigationOnClickListener {
+            lifecycleScope.launch(Dispatchers.IO) {
+                val intent = Intent(requireContext(), HomeActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+                withContext(Dispatchers.Main) {
+                    startActivity(intent)
+                    requireActivity().finish()
+                }
+            }
+        }
 
         val textView: TextView = binding.textData
         dataViewModel.text.observe(viewLifecycleOwner) {
